e2d1a70fc7132823f12ae56ec7d38cdb37cb4cc6
Tur.4000 <tur.4000@gmail.com>
2016-09-22 00:12:42 +0300

    Создали класс Snake и перечисление Direction

--- snake/Program.cs
+++ snake/Program.cs
@@ -23,7 +23,8 @@ namespace snake
             lineVl.Drow();
 
             Point p = new Point(4, 5, '@');
-            p.Draw();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
 
             Console.ReadLine();
         }        
