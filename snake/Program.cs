160e9c30cdcea075159c70480c59643aaf2c1b8a
Tur.4000 <tur.4000@gmail.com>
2016-09-21 22:52:32 +0300

    Домашка 7-го урока. Создал класс рисующий вертикальную линию

--- snake/Program.cs
+++ snake/Program.cs
@@ -16,8 +16,11 @@ namespace snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            HorizontalLine line = new HorizontalLine(5, 11, 8, '+');
-            line.Drow();
+            HorizontalLine lineH = new HorizontalLine(7, 40, 8, '+');
+            lineH.Drow();
+
+            VerticalLine lineV = new VerticalLine(4, 20, 5, '#');
+            lineV.Drow();
 
             Console.ReadLine();
         }        
