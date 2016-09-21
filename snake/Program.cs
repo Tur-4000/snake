27901370c90077308ed09cf2622fc39378dbdb9b
Tur.4000 <tur.4000@gmail.com>
2016-09-21 21:09:36 +0300

    домашка 6-го урока

--- snake/Program.cs
+++ snake/Program.cs
@@ -13,9 +13,32 @@ namespace snake
             Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
-            Point p2 = new Point(4, 5, '#');
+            Point p2 = new Point(1, 2, '#');
             p2.Draw();
 
+            List<char> chList = new List<char>();
+            chList.Add('*');
+            chList.Add('#');
+            chList.Add('@');
+            chList.Add('*');
+            chList.Add('*');
+
+            Point p3 = new Point(2, 3, chList[2]);
+            Point p4 = new Point(3, 4, chList[2]);
+            Point p5 = new Point(4, 5, chList[2]);
+            Point p6 = new Point(5, 6, chList[2]);
+
+            p3.Draw();
+            p4.Draw();
+            p5.Draw();
+            p6.Draw();
+
+            List<Point> p1List = new List<Point>();
+            p1List.Add(p3);
+            p1List.Add(p4);
+            p1List.Add(p5);
+            p1List.Add(p6);
+            
             List<int> numList = new List<int>();
             numList.Add(0);
             numList.Add(1);
