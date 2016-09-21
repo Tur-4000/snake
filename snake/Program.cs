7d73b04b79cc987367b7786873431dfb23cf9174
Tur.4000 <tur.4000@gmail.com>
2016-09-21 22:23:02 +0300

    Создали класс выводящий горизонтальную линию

--- snake/Program.cs
+++ snake/Program.cs
@@ -16,7 +16,7 @@ namespace snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            HorizontalLine line = new HorizontalLine();
+            HorizontalLine line = new HorizontalLine(5, 11, 8, '+');
             line.Drow();
 
             Console.ReadLine();
